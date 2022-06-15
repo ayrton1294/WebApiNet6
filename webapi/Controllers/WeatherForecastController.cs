0367d4dd7960b1aedcb5ddf25830e9f648923a7b
Ayrton La Madrid <ayrtoncesar12@gmail.com>
2022-06-15 02:57:48 -0500

    Atributos para verbos http controlador

---
+++ webapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            if(ListWeatherForecast == null || !ListWeatherForecast.Any() || ListWeatherForecast.Count() == 0)
+            {
+                ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                }).ToList();
+            }
+        }
+
+        [HttpGet("GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return ListWeatherForecast;
+        }
+
+        [HttpPost("PostWeatherForecast")]
+        public IActionResult Post(WeatherForecast weatherForecast)
+        {
+            ListWeatherForecast.Add(weatherForecast);
+            return Ok();
+        }
+
+        [HttpDelete("DeleteWeatherForecast/{index}")]
+        public IActionResult Delete(int index)
+        {
+            ListWeatherForecast.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
